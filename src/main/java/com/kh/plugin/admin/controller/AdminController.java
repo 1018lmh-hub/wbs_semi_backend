aff3dfd4d1ba096deaa1744124939f91d085fb5f
CU0-0 <bill97720@naver.com>
2026-07-02 16:33:41 +0900

    feat: merge 전

--- src/main/java/com/kh/plugin/admin/controller/AdminController.java
+++ src/main/java/com/kh/plugin/admin/controller/AdminController.java
@@ -1,14 +1,10 @@
-package com.kh.plugin.noticeboard.controller;
-
-import java.util.List;
+package com.kh.plugin.admin.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.plugin.common.model.vo.ApiResponse;
@@ -20,26 +16,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/notices")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
-public class NoticeBoardController {
-	
+public class AdminController {
+
 	private final NoticeBoardService noticeBoardService;
 	
-	@GetMapping
-	public ResponseEntity<ApiResponse<List<NoticeBoardResponseDto>>> findAll(@RequestParam(name="page", defaultValue = "1") int page){
-		List<NoticeBoardResponseDto> notices = noticeBoardService.findAll(page);
-		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(notices));
-	}
-	
-	@GetMapping("/{noticeNo}")
-	public ResponseEntity<ApiResponse<NoticeBoardResponseDto>> findByNoticeNo(@PathVariable(value="noticeNo") Long noticeNo){
+	@PostMapping("/{noticeNo}")
+	public ResponseEntity<ApiResponse<Void>> save(@PathVariable(value="noticeNo") Long noticeNo){
 		NoticeBoardResponseDto notices = noticeBoardService.findByNoticeNo(noticeNo);
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(notices));
 	}
-	
-	
-	
-	
-
 }
