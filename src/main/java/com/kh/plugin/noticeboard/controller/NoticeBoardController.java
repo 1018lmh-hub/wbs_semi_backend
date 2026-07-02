002755290f566e100c96ef18e0a5a2effe706d37
CU0-0 <bill97720@naver.com>
2026-07-02 10:27:43 +0900

    feat: 공지사항 전체보기 controller구성

--- src/main/java/com/kh/plugin/noticeboard/controller/NoticeBoardController.java
+++ src/main/java/com/kh/plugin/noticeboard/controller/NoticeBoardController.java
@@ -1,5 +1,33 @@
 package com.kh.plugin.noticeboard.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.plugin.common.model.vo.ApiResponse;
+import com.kh.plugin.noticeboard.model.dto.NoticeBoardResponseDto;
+import com.kh.plugin.noticeboard.model.service.NoticeBoardService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/notices")
+@RequiredArgsConstructor
 public class NoticeBoardController {
+	
+	private final NoticeBoardService noticeBoardService;
+	
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<NoticeBoardResponseDto>>> findAll(@RequestParam(value="page", defaultValue = "1") int page){
+		NoticeBoardResponseDto notices = noticeBoardService.findAll(page);
+		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(notices));
+	}
 
 }
--- src/main/java/com/kh/plugin/noticeboard/model/service/NoticeBoardService.java
+++ src/main/java/com/kh/plugin/noticeboard/model/service/NoticeBoardService.java
@@ -1,5 +1,13 @@
 package com.kh.plugin.noticeboard.model.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class NoticeBoardService {
 
 }
