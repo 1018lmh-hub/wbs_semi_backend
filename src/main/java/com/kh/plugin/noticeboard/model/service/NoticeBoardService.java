4f894dc2c85db70ee6af4b2a3120687b50bd5f33
CU0-0 <bill97720@naver.com>
2026-07-02 10:35:38 +0900

    feat: 공지사항 전체보기 service 구성

--- src/main/java/com/kh/plugin/noticeboard/model/service/NoticeBoardService.java
+++ src/main/java/com/kh/plugin/noticeboard/model/service/NoticeBoardService.java
@@ -1,6 +1,12 @@
 package com.kh.plugin.noticeboard.model.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kh.plugin.common.model.dto.PageInfo;
+import com.kh.plugin.common.util.Pagination;
+import com.kh.plugin.noticeboard.model.dao.NoticeBoardMapper;
+import com.kh.plugin.noticeboard.model.dto.NoticeBoardResponseDto;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,5 +15,20 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class NoticeBoardService {
+	
+	private final NoticeBoardMapper noticeBoardMapper;
+	private final Pagination pagination;
+	
+	@Transactional
+	public NoticeBoardResponseDto findAll(int page) {
+		PageInfo pi = pagination.getPageInfo(countNotices(), page, 5, 5);
+		NoticeBoardResponseDto notices = noticeBoardMapper.findAll(pi);
+		return notices;
+	}
+	
+	
+	private int countNotices() {
+		return noticeBoardMapper.countNotices();
+	}
 
 }
