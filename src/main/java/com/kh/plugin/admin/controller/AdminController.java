2e49dd9e48fbbd57ddc62d239eade59f7670a07b
CU0-0 <bill97720@naver.com>
2026-07-02 18:02:44 +0900

    feat: 공지사항 작성하기

--- src/main/java/com/kh/plugin/admin/controller/AdminController.java
+++ src/main/java/com/kh/plugin/admin/controller/AdminController.java
@@ -2,15 +2,17 @@ package com.kh.plugin.admin.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.plugin.auth.model.vo.CustomUserDetails;
 import com.kh.plugin.common.model.vo.ApiResponse;
-import com.kh.plugin.noticeboard.model.dto.NoticeBoardResponseDto;
+import com.kh.plugin.noticeboard.model.dto.SaveNoticeBoardDto;
 import com.kh.plugin.noticeboard.model.service.NoticeBoardService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,9 +24,5 @@ public class AdminController {
 
 	private final NoticeBoardService noticeBoardService;
 	
-	@PostMapping("/{noticeNo}")
-	public ResponseEntity<ApiResponse<Void>> save(@PathVariable(value="noticeNo") Long noticeNo){
-		noticeBoardService.findByNoticeNo(noticeNo);
-		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(null));
-	}
+
 }
