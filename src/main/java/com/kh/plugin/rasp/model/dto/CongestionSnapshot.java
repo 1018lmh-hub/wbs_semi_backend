f3902eca981e07cda5a0f1afb0b46405489c1152
CU0-0 <bill97720@naver.com>
2026-07-13 16:25:53 +0900

    feat: 라즈베리파이 수정

---
+++ src/main/java/com/kh/plugin/rasp/model/dto/CongestionSnapshot.java
@@ -0,0 +1,14 @@
+package com.kh.plugin.rasp.model.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import com.kh.plugin.rasp.model.vo.Device;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CongestionSnapshot {
+    private final LocalDateTime asOf; 
+    private final List<Device> devices;
+}
